1e91e0b3124b708575be18afb08a188216a7ab49
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2021-12-18 15:39:02 +1000

    oops...

--- pkg/api/handlers/services.go
+++ pkg/api/handlers/services.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	mcontext "maestro/pkg/api/context"
 	"maestro/pkg/model"
 	"maestro/pkg/streamingService"
@@ -17,8 +16,6 @@ func ListServices(w http.ResponseWriter, r *http.Request) {
 	}
 
 	res, err := container.ResolveWithResult(func(cfg streamingService.Config) ([]model.StreamingService, error) {
-
-		return nil, fmt.Errorf("fuck you")
 
 		var res []model.StreamingService
 		for k, c := range cfg {
