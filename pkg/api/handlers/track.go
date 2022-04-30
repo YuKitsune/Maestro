e967a195aae49220dad6c5af6bafef44aec85672
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2022-04-30 14:03:08 +1000

    Find missing tracks when querying ISRC

--- pkg/api/handlers/track.go
+++ pkg/api/handlers/track.go
@@ -3,9 +3,11 @@ package handlers
 import (
 	"context"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	mcontext "github.com/yukitsune/maestro/pkg/api/context"
 	"github.com/yukitsune/maestro/pkg/api/db"
 	"github.com/yukitsune/maestro/pkg/model"
+	"github.com/yukitsune/maestro/pkg/streamingservice"
 	"net/http"
 )
 
@@ -24,10 +26,28 @@ func HandleGetTrackByIsrc(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	t, err := container.ResolveWithResult(func(ctx context.Context, repo db.Repository) (interface{}, error) {
+	t, err := container.ResolveWithResult(func(ctx context.Context, repo db.Repository, svcs []streamingservice.StreamingService, logger *logrus.Entry) (interface{}, error) {
 		foundTracks, err := repo.GetTracksByIsrc(ctx, isrc)
 		if err != nil {
 			return nil, err
+		}
+
+		if len(foundTracks) != len(svcs) {
+			newTracks, err := getNewTrackByIsrc(isrc, foundTracks, svcs)
+			if err != nil {
+				return nil, err
+			}
+
+			n, err := repo.AddTracks(ctx, newTracks)
+			if err != nil {
+				return nil, err
+			}
+
+			logger.Infof("%d new tracks added", n)
+
+			for _, newTrack := range newTracks {
+				foundTracks = append(foundTracks, newTrack)
+			}
 		}
 
 		return foundTracks, nil
@@ -48,4 +68,37 @@ func HandleGetTrackByIsrc(w http.ResponseWriter, r *http.Request) {
 	res.AddAll(model.TrackToHasStreamingServiceSlice(tracks))
 
 	Response(w, res, http.StatusOK)
+}
+
+func getNewTrackByIsrc(isrc string, knownTracks []*model.Track, svcs []streamingservice.StreamingService) ([]*model.Track, error) {
+
+	var tracks []*model.Track
+
+	for _, svc := range svcs {
+
+		// Skip if we know about this track
+		trackIsKnown := false
+		for _, knownTrack := range knownTracks {
+			if knownTrack.Source == svc.Key() {
+				trackIsKnown = true
+			}
+		}
+
+		if trackIsKnown {
+			continue
+		}
+
+		track, found, err := svc.GetTrackByIsrc(isrc)
+		if err != nil {
+			return tracks, err
+		}
+
+		if !found {
+			continue
+		}
+
+		tracks = append(tracks, track)
+	}
+
+	return tracks, nil
 }
